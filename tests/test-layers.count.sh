9ea89a84d93190788d9f3b3578a1277cb4a43f56
zlocate <zlocated@gmail.com>
2022-09-05 23:24:06 +0300

    feat: added build pipeline

---
+++ tests/test-layers.count.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+LAYER_COUNT_LIMIT=$1
+IMAGE_LAYERS_COUNT=`docker image inspect my-app -f '{{.RootFS.Layers}}' | wc -w`
+
+if [ "$IMAGE_LAYERS_COUNT" -le "$LAYER_COUNT_LIMIT" ]
+then
+    echo "Отличная работа. Количество слоев $IMAGE_LAYERS_COUNT при допустимых $LAYER_COUNT_LIMIT"
+    exit 1
+else
+    echo "К сожалению, количество слоев $IMAGE_LAYERS_COUNT больше чем разрешено - $LAYER_COUNT_LIMIT"
+    exit 0
+fi
